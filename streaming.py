02b7625c0afef6e96ed1b5d7ac9518629044f180
Jon Wiggins <me@jonwiggins.org>
2019-09-29 18:02:18 -0600

    update file names to lowercase

---
+++ streaming.py
@@ -0,0 +1,23 @@
+"""
+One day I will implement these two Streaming algorithms here
+Until then, feel free to call these methods just for fun!
+"""
+
+__author__ = "Jon Wiggins"
+
+import numpy as np
+
+
+class MisaGries:
+    """
+    TODO add
+    """
+    pass
+
+
+class CountMinSketch:
+    """
+    TODO add
+    """
+    pass
+
